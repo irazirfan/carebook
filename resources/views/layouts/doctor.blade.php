c30ee6a02a7bc6090096f7b149650c4b80333697
tratul <34869130+tratul@users.noreply.github.com>
2019-08-27 01:08:56 +0600

    Doctro Meta tag edit

--- resources/views/layouts/doctor.blade.php
+++ resources/views/layouts/doctor.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <title>Doctor | Carebook</title>
     @yield('style')
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
