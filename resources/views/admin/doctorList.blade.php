6256854e68caa68b2f272d251e9bbfe73b05183e
Iraz Irfan <irazirfan@gmail.com>
2019-08-27 03:57:59 +0600

    Admin dashboard front-end updated

---
+++ resources/views/admin/doctorList.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.admin')
+@section('content')
+
+    <section id="main-content">
+        <section class="wrapper">
+            <h3><i class="fa fa-angle-right"></i>Doctor List</h3>
+            <div class="row">
+            <div class="row mt">
+                <div class="col-md-12">
+                    <div class="content-panel">
+                        <table class="table table-striped table-advance table-hover">
+
+                            <thead>
+                            <tr>
+                                <th><i class="fa fa-bullhorn"></i> Company</th>
+                                <th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
+                                <th><i class="fa fa-bookmark"></i> Profit</th>
+                                <th><i class=" fa fa-edit"></i> Status</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    <a href="basic_table.html#">Company Ltd</a>
+                                </td>
+                                <td class="hidden-phone">Lorem Ipsum dolor</td>
+                                <td>12000.00$ </td>
+                                <td><span class="label label-info label-mini">Due</span></td>
+                                <td>
+                                    <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="basic_table.html#">
+                                        Dashio co
+                                    </a>
+                                </td>
+                                <td class="hidden-phone">Lorem Ipsum dolor</td>
+                                <td>17900.00$ </td>
+                                <td><span class="label label-warning label-mini">Due</span></td>
+                                <td>
+                                    <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="basic_table.html#">
+                                        Another Co
+                                    </a>
+                                </td>
+                                <td class="hidden-phone">Lorem Ipsum dolor</td>
+                                <td>14400.00$ </td>
+                                <td><span class="label label-success label-mini">Paid</span></td>
+                                <td>
+                                    <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="basic_table.html#">Dashio ext</a>
+                                </td>
+                                <td class="hidden-phone">Lorem Ipsum dolor</td>
+                                <td>22000.50$ </td>
+                                <td><span class="label label-success label-mini">Paid</span></td>
+                                <td>
+                                    <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="basic_table.html#">Total Ltd</a>
+                                </td>
+                                <td class="hidden-phone">Lorem Ipsum dolor</td>
+                                <td>12120.00$ </td>
+                                <td><span class="label label-warning label-mini">Due</span></td>
+                                <td>
+                                    <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                                    <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /content-panel -->
+                </div>
+                <!-- /col-md-12 -->
+            </div>
+            <!-- /row -->
+        </section>
+    </section>
+
+    </section>
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="lib/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="lib/jquery.scrollTo.min.js"></script>
+    <script src="lib/jquery.nicescroll.js" type="text/javascript"></script>
+    <!--common script for all pages-->
+    <script src="lib/common-scripts.js"></script>
+    <!--script for this page-->
+
+    </body>
+
+@endsection
