b80557b5f8424a4d5449e108e210a18c4759d079
tratul <34869130+tratul@users.noreply.github.com>
2019-09-14 22:36:57 +0600

    Revert "Revert "Signup Doctor.""
    This reverts commit 15f269fdb1a5eb1bd396f33cbdedd64426b94e0e.

--- app/Doctor.php
+++ app/Doctor.php
@@ -12,5 +12,5 @@ class Doctor extends Model
     protected $table = 'doctor';
     //protected $primaryKey ='email' ;
 
-    protected $fillable = ['email', 'bmdc'];
+    protected $fillable = ['email', 'bmdc', 'degree', 'specialized', 'consulting', 'location', 'contact'];
 }
